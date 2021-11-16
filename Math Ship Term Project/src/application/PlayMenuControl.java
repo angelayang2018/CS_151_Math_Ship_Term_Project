a90e5379d19520e6fd6b343ea715dbb6487deb1a
razzacktiger <harrazzack@gmail.com>
2021-11-15 23:03:29 -0800

    updated project with latest assignemnt milestone submission

--- Math Ship Term Project/src/application/PlayMenuControl.java
+++ Math Ship Term Project/src/application/PlayMenuControl.java
@@ -10,6 +10,7 @@ import javafx.animation.AnimationTimer;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Cursor;
 import javafx.scene.Node;
@@ -17,6 +18,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
@@ -33,52 +35,48 @@ public class PlayMenuControl {
 	private Stage stage;
 	private Scene scene;
 	private Parent root; 
-	Ship player;
-	List<Shot>shots;
-	List<Meteor>Meteors;
+	private Ship player;
+	private List<Bullet>bullets;
+	private List<Meteor>meteors;
 	private static final Random RAND = new Random();
-	private static final int WIDTH = 500;
+	private static final int WIDTH = 500; //width and height of canvas
 	private static final int HEIGHT = 700;
-	private static final int PLAYER_SIZE = 50;
-
-	private double mouseX;
-	private double mouseY;
-	static int score;
-	static int inequality;
+	private static final int PLAYER_SIZE = 50; //this will be basically the hit box of player
+	private double mouseX; 
+	private static int score;
+	private static int inequality;
 	public GraphicsContext gc;
+	
     static final Image PLAYER_IMG = new Image("https://i.ibb.co/PD1KSQp/Red-Fly-Ship.png");
     static final Image REDMETEOR = new Image("https://i.ibb.co/k63WNzJ/Multiply-Fire.png");
     static final Image BLUEMETEOR = new Image("https://i.ibb.co/5r8zbBF/divideball.png");
     static final Image PINKMETEOR = new Image("https://i.ibb.co/HTsc3zQ/AddBall.png");
     static final Image GREENMETEOR = new Image("https://i.ibb.co/gZLqLGM/Subtract-Ball.png");
-    static final Image background = new Image("https://i.ibb.co/tpRfC5H/CommonBG.png");
-    //ImageView bg = new ImageView(background);
+    static final Image background = new Image("https://i.ibb.co/tpRfC5H/CommonBG.png"); //need to work on background
     
-    static final Image BOMBS_IMG[]= {
-    		REDMETEOR, BLUEMETEOR, PINKMETEOR, GREENMETEOR
-    };
-	final int MAX_BOMBS = 10,  MAX_SHOTS = MAX_BOMBS * 2;
+    static final Image METEOR_IMGS[]= {REDMETEOR, BLUEMETEOR, PINKMETEOR, GREENMETEOR }; //Meteor Images in array
+	final int MAX_METEORS = 10,  MAX_SHOTS = MAX_METEORS * 2;
 	boolean gameOver = false;
 	
 	public void startGame(ActionEvent event) throws IOException {
 		
 		stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
 		stage.setTitle("Math Ship");
-
-        Canvas canvas = new Canvas(500, 700);
+        Canvas canvas = new Canvas(WIDTH, HEIGHT);
         gc = canvas.getGraphicsContext2D();
-
-        BorderPane root = new BorderPane(canvas);
-        Scene mainScene = new Scene(root, 500, 700);
-       
+        BorderPane root1 = new BorderPane(canvas);
+        Scene mainScene = new Scene(root1, WIDTH, HEIGHT);
+        
+ 
+        //Created time line object with Duration set at 10 ms. it will run the Graphics Context
         Timeline timeline = new Timeline(new KeyFrame(Duration.millis(10), e -> run(gc)));
         timeline.setCycleCount(Timeline.INDEFINITE);
         timeline.play();
         canvas.setCursor(Cursor.MOVE);
         canvas.setOnMouseMoved(e -> mouseX = e.getX()); //gets positions of x of mouse cursor
         canvas.setOnMousePressed(e -> {
-			if(shots.size() < MAX_SHOTS) { //shots cannot exceed twice the amount of enemy objects
-				shots.add(player.shoot()); 
+			if(bullets.size() < MAX_SHOTS) { //shots cannot exceed twice the amount of enemy objects
+				bullets.add(player.shoot()); 
 			}
 			if(gameOver) { 
 				gameOver = false;
@@ -90,6 +88,7 @@ public class PlayMenuControl {
         stage.setScene(mainScene);
         stage.setResizable(false);
         stage.show();
+ 
 	}
 	
 	private void run(GraphicsContext gc) {
@@ -103,45 +102,41 @@ public class PlayMenuControl {
 		gc.setFont(Font.font(15));
 		gc.setFill(Color.WHITE);
 		gc.fillText("Score: " + score, 60,  20);
-		gc.fillText("Inequality: " + inequality + " < 100", 250, 20);
+		gc.setFont(Font.font(30));
+		gc.setFill(Color.YELLOW);
+		gc.fillText(inequality + " < 100", 250, 40);
 		
 		if(gameOver || inequality > 100) { //If player dies or makes the inequality false
 			gameOver = true;
 			gc.setFont(Font.font(35));
-			gc.setFill(Color.YELLOW);
+			gc.setFill(Color.RED);
 			gc.fillText("Game Over \n Your Score: " + score + "\n\n Click to Play Again", WIDTH / 2, HEIGHT /2.5);
-			
 		}
 		else {
-			playGame();
-		}
-	}//end run()
-	
-	private void playGame(){
 		player.update();
-		player.draw();
+		player.draw(); //draws the players image
 		
 
-		if(mouseX < WIDTH - PLAYER_SIZE) {
+		if(mouseX < WIDTH - PLAYER_SIZE) {//this is here so that the player can't move off screen
 		player.posX = (int) mouseX;
 		}
 		
-		Meteors.stream().peek(Ship::update).peek(Ship::draw).forEach(e -> {
-			if(player.colide(e) && !player.exploding) {
+		meteors.stream().peek(Ship::update).peek(Ship::draw).forEach(e -> { //checks if player collides 
+			if(player.collide(e) && !player.exploding) {
 				player.explode();
 			}
 		});
 		
-		for (int i = shots.size() - 1; i >=0 ; i--) {
-			Shot shot = shots.get(i);
-			if(shot.posY < 0 || shot.toRemove)  { 
-				shots.remove(i);
+		for (int i = bullets.size() - 1; i >=0 ; i--) {
+			Bullet shot = bullets.get(i);
+			if(shot.posY < 0 || shot.toRemove)  { //checks if bullet collides or if it goes off screen
+				bullets.remove(i);
 				continue;
 			}
 			shot.update();
 			shot.draw();
 			
-			for (Meteor bomb : Meteors) {
+			for (Meteor bomb : meteors) { //METEOR MATH COLLIDING LOGIC PINK(add) RED(MULT) GREEN(SUB) PURPLE(divide)
 				if(shot.colide(bomb) && !bomb.exploding && bomb.img == PINKMETEOR) {
 					inequality++;
 					score++;
@@ -157,6 +152,7 @@ public class PlayMenuControl {
 					shot.toRemove = true;
 				}
 				else if(shot.colide(bomb) && !bomb.exploding && bomb.img == REDMETEOR) {
+					
 					inequality = inequality*2;
 					score++;
 					bomb.explode();
@@ -172,24 +168,33 @@ public class PlayMenuControl {
 				}
 			}
 		}
-		for (int i = Meteors.size() - 1; i >= 0; i--){  
-			if(Meteors.get(i).destroyed)  {
-				Meteors.set(i, newBomb());
+		
+		
+		for (int i = meteors.size() - 1; i >= 0; i--){ //go through meteor list if it is destroyed create a new one  
+			if(meteors.get(i).destroyed)  {
+				meteors.set(i, newBomb());
 			}
 		}
-		gameOver = player.destroyed;
-	}//end playGame()
+		
+		
+		gameOver = player.destroyed; //if player is destroyed then the game is over
+
+		}
+		
+
+	}
 	
 	private void setup() {
-		shots = new ArrayList <>();
-		Meteors = new ArrayList<>();
+		bullets = new ArrayList <>();
+		meteors = new ArrayList<>();
         player = new Ship(250, 650, PLAYER_SIZE, PLAYER_SIZE, PLAYER_IMG);
         score = 0;
         inequality = 1;
-		IntStream.range(0, MAX_BOMBS).mapToObj(i -> this.newBomb()).forEach(Meteors::add);
+		IntStream.range(0, MAX_METEORS).mapToObj(i -> this.newBomb()).forEach(meteors::add);
 
 	}
-	public class Ship{
+	
+	public class Ship{ //Ship Player class
 
 		int posX, posY, sizeX, sizeY;
 		Image img;
@@ -213,11 +218,11 @@ public class PlayMenuControl {
 			
 		}
 		
-		public Shot shoot() {
-			return new Shot(posX + sizeX / 2 - Shot.size / 2, posY - Shot.size);
+		public Bullet shoot() {
+			return new Bullet(posX + sizeX / 2 - Bullet.size / 2, posY - Bullet.size);
 		}
 		
-		public boolean colide(Ship other) {
+		public boolean collide(Ship other) {
 			int d = distance(this.posX + sizeX / 2, this.posY + sizeY /2, 
 							other.posX + other.sizeX / 2, other.posY + other.sizeY / 2);
 			return d < other.sizeX / 2 + this.sizeX / 2 ;
@@ -228,9 +233,13 @@ public class PlayMenuControl {
 		}
 	}
 
+	
+	
+	
+	
 	public class Meteor extends Ship {
 		
-		int SPEED = (score/100)+2;
+		int SPEED = (score/100)+2; //can change this to adjust difficulty
 		
 		public Meteor(int posX, int posY, int sizeX, int sizeY, Image image) {
 			super(posX, posY, sizeX, sizeY, image);
@@ -248,14 +257,14 @@ public class PlayMenuControl {
 		}
 	}
 	
-	public class Shot{
+	public class Bullet{
 		
 		public boolean toRemove;
 
 		int posX, posY, speed = 10;
 		static final int size = 6;
 			
-		public Shot(int posX, int posY) {
+		public Bullet(int posX, int posY) {
 			this.posX = posX;
 			this.posY = posY;
 		}
@@ -279,18 +288,18 @@ public class PlayMenuControl {
 
 	}
 	
-	public Meteor newBomb() {
-		return new Meteor(50 + RAND.nextInt(WIDTH - 100), 0, 50, 80, BOMBS_IMG[RAND.nextInt(BOMBS_IMG.length)]);
+	public Meteor newBomb() { //generates random bomb at a random x location
+		return new Meteor(50 + RAND.nextInt(WIDTH - 100), 0, 50, 80, METEOR_IMGS[RAND.nextInt(METEOR_IMGS.length)]);
 	}
 	
 	
-	int distance(int x1, int y1, int x2, int y2) {
+	int distance(int x1, int y1, int x2, int y2) { //Distance formula
 		return (int) Math.sqrt(Math.pow((x1 - x2), 2) + Math.pow((y1 - y2), 2));
 	}
 	
 
 	
-	//Scene switchers below: 
+	
 	public void toScoreboard(ActionEvent event) throws IOException {
 		root = FXMLLoader.load(getClass().getResource("view/ScoreboardScene.fxml"));
 		stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
--- Math Ship Term Project/src/application/SignUpControl.java
+++ Math Ship Term Project/src/application/SignUpControl.java
@@ -38,7 +38,6 @@ public class SignUpControl {
 	
 	public void inputCredentials(){
 		
-		
 	}
 	
 
